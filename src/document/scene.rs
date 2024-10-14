123cff0c0a520d9f7a2b0b7d34fc4b5734569b1b
franciscowxp <franciscowxp@gmail.com>
2024-10-14 10:49:01 +0800

    feat: change the scene struct

--- src/document/scene.rs
+++ src/document/scene.rs
@@ -3,11 +3,9 @@ use struct_field_names_as_array::FieldNamesAsSlice;
 
 use crate::utils::GenOptionValue;
 
-use super::{LinkRole, LinkRoleFilter};
-
 #[derive(Serialize, Deserialize, Debug, Clone, FieldNamesAsSlice)]
 pub struct Scene {
-  pub role: String,
+  pub role: Option<Vec<String>>,
   value: String,
   image: String,
   create_timestamp: Option<i64>,
@@ -15,11 +13,3 @@ pub struct Scene {
 }
 
 impl GenOptionValue for Scene {}
-
-impl LinkRole for Scene {
-  fn role(&self) -> String {
-    self.role.clone()
-  }
-}
-
-impl LinkRoleFilter<Scene> for Vec<Scene> {}
