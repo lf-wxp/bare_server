95f60dcdca684c34ba5aa1c6b33f5933465041ab
franciscowxp <franciscowxp@gmail.com>
2024-10-14 10:20:46 +0800

    feat: add scene

--- src/document/scene.rs
+++ src/document/scene.rs
@@ -3,8 +3,11 @@ use struct_field_names_as_array::FieldNamesAsSlice;
 
 use crate::utils::GenOptionValue;
 
+use super::{LinkRole, LinkRoleFilter};
+
 #[derive(Serialize, Deserialize, Debug, Clone, FieldNamesAsSlice)]
 pub struct Scene {
+  pub role: String,
   value: String,
   image: String,
   create_timestamp: Option<i64>,
@@ -12,3 +15,11 @@ pub struct Scene {
 }
 
 impl GenOptionValue for Scene {}
+
+impl LinkRole for Scene {
+  fn role(&self) -> String {
+    self.role.clone()
+  }
+}
+
+impl LinkRoleFilter<Scene> for Vec<Scene> {}
